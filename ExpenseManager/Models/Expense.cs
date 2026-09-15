2143fba77eb3c36d941a92e207d8dcce947dd7a2
TomaszWojcicki <Axerrek26@gmail.com>
2026-09-15 07:54:46 +0200

    Utworzenie walidacji danych

--- ExpenseManager/Models/Expense.cs
+++ ExpenseManager/Models/Expense.cs
@@ -1,4 +1,6 @@
-﻿namespace ExpenseManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseManager.Models
 {
 
     public enum ExpenseCategory
@@ -10,9 +12,30 @@
     public class Expense
     {
         public int Id { get; set; }
-        public ExpenseCategory Category { get; set; }
+        [Required(ErrorMessage = "Opis jest wymagany.")]
+        [StringLength(100, ErrorMessage = "Opis nie może przekraczać 100 znaków.")]
+        [Display(Name = "Opis wydatku")]
         public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+
+        private decimal? _price;
+
+        [Required(ErrorMessage = "Kwota jest wymagana.")]
+        [Range(0.01, 1000000.00, ErrorMessage = "Kwota musi być większa od 0.")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Kwota")]
+        public decimal? Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        [Required(ErrorMessage = "Wybierz kategorię.")]
+        [Display(Name = "Kategoria")]
+        public ExpenseCategory Category { get; set; }
+
+        [Required(ErrorMessage = "Data jest wymagana.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data")]
         public DateTime Date { get; set; } = DateTime.Now;
 
     }
